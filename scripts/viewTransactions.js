8137f1e1bb3f53d98adffbdb98d2d5f0485f2ff2
RiyandiDjohari <riyandidjohari8@gmail.com>
2024-04-03 06:35:47 +0800

    feat: add function view transaction

--- scripts/viewTransactions.js
+++ scripts/viewTransactions.js
@@ -1,6 +1,16 @@
-const { accounts } = require('./accounts');
-const { user } = require('./user');
+const { accounts } = require("./accounts");
+const { user } = require("./user");
 
-function viewTransactions() {}
+function viewTransactions(account) {
+  const { transactions } = account.transactions;
+
+  console.log("Riwayat Transaksi:");
+
+  if (transactions.length === 0) {
+    console.log("Belum ada transaksi.");
+  } else {
+    transactions.map((transaction, i) => console.log(`${i + 1}. ${transaction.type}: Rp ${transaction.amount}`));
+  }
+}
 
 module.exports = { viewTransactions };
