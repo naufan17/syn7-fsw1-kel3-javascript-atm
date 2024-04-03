c3f021dc0ce0ab68cd5530ed0b4a070aa8e6ffa0
Aji Nuansa Sengarie <ansengarie@gmail.com>
2024-04-03 16:44:45 +0700

    feat: add deposit function

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
-const { login } = require('./scripts/login');
-const { logout } = require('./scripts/logout');
-const { checkBalance } = require('./scripts/checkBalance');
-const { deposit } = require('./scripts/deposit');
-const { viewTransactions } = require('./scripts/viewTransactions');
-const { askQuestion, rl } = require('./scripts/utils');
+const { login } = require("./scripts/login");
+const { logout } = require("./scripts/logout");
+const { checkBalance } = require("./scripts/checkBalance");
+const { deposit } = require("./scripts/deposit");
+const { viewTransactions } = require("./scripts/viewTransactions");
+const { askQuestion, rl } = require("./scripts/utils");
 
 async function main() {
   try {
@@ -11,22 +11,32 @@ async function main() {
     let choice;
 
     do {
-      console.log('======================')
-      console.log('Menu ATM:');
-      console.log('1. Cek Saldo'); 
-      console.log('2. Setor Tunai');
-      console.log('3. Riwayat Transaksi');
-      console.log('4. Keluar');
-  
-      choice = await askQuestion('Masukkan pilihan Anda: ');
-  
+      console.log("======================");
+      console.log("Menu ATM:");
+      console.log("1. Cek Saldo");
+      console.log("2. Setor Tunai");
+      console.log("3. Riwayat Transaksi");
+      console.log("4. Keluar");
+
+      choice = await askQuestion("Masukkan pilihan Anda: ");
+
       switch (parseInt(choice)) {
         case 1:
-          checkBalance(account)
+          checkBalance(account);
           break;
         case 2:
-          deposit()
-          break;
+          const amount = parseFloat(
+            await askQuestion("Masukkan nominal setoran Anda: ")
+          );
+          if (amount <= 0 || isNaN(amount)) {
+            console.log("Nominal tidak valid");
+            break;
+          }
+          deposit(account, amount);
+          console.log("Sedang memproses transaksi...");
+          console.log(
+            "Setoran berhasil. Saldo Anda sekarang: " + checkBalance(account)
+          );
         case 3:
           viewTransactions(account);
           break;
@@ -34,10 +44,10 @@ async function main() {
           logout();
           break;
       }
-    } while (choice !== 4);  
+    } while (choice !== 4);
   } catch (err) {
     rl.close();
   }
 }
 
-main(); 
+main();
