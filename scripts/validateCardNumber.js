f72f2c3e39c8723fc5370ddbc1e67cddca9cf101
Mardhyah <mardhyahfathaniaizzati9d@gmail.com>
2024-04-03 07:09:18 +0700

    feat: add function validateCardNumber

--- scripts/validateCardNumber.js
+++ scripts/validateCardNumber.js
@@ -1,5 +1,11 @@
 const { accounts } = require('./accounts');
 
-function validateCardNumber() {}
+function validateCardNumber(cardNumber) {
+    const account = accounts.find(acc => acc.cardNumber === cardNumber);
+    if (!account) {
+        throw new Error('Nomor kartu tidak valid.');
+    }
+    return account;
+}
 
 module.exports = { validateCardNumber };
